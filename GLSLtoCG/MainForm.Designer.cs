4fa348998071a780e5c7bb640935a10fb2395c08
eddietree <eddie@illogictree.com>
2014-01-23 15:08:16 +0900

    changed a bit the form

--- GLSLtoCG/MainForm.Designer.cs
+++ GLSLtoCG/MainForm.Designer.cs
@@ -39,27 +39,27 @@
             this.btn_drop_files.Dock = System.Windows.Forms.DockStyle.Top;
             this.btn_drop_files.Location = new System.Drawing.Point(0, 0);
             this.btn_drop_files.Name = "btn_drop_files";
-            this.btn_drop_files.Size = new System.Drawing.Size(284, 149);
+            this.btn_drop_files.Size = new System.Drawing.Size(439, 149);
             this.btn_drop_files.TabIndex = 0;
-            this.btn_drop_files.Text = "Drop Shaders Here";
+            this.btn_drop_files.Text = "Drop GLSL Shaders Here";
             this.btn_drop_files.UseVisualStyleBackColor = true;
             this.btn_drop_files.Click += new System.EventHandler(this.btn_drop_files_Click);
             // 
             // textbox_log
             // 
-            this.textbox_log.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textbox_log.Dock = System.Windows.Forms.DockStyle.Fill;
             this.textbox_log.Enabled = false;
             this.textbox_log.Location = new System.Drawing.Point(0, 149);
             this.textbox_log.Multiline = true;
             this.textbox_log.Name = "textbox_log";
-            this.textbox_log.Size = new System.Drawing.Size(284, 112);
+            this.textbox_log.Size = new System.Drawing.Size(439, 128);
             this.textbox_log.TabIndex = 1;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(439, 277);
             this.Controls.Add(this.textbox_log);
             this.Controls.Add(this.btn_drop_files);
             this.Name = "MainForm";
